1f0cb0a953d0a233be0b9dbbfb267e08f34fe4ac
Taeyeon <lty0314@gmail.com>
2024-10-29 10:46:57 +0900

    build: package추가 #3

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
 import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'snijffzocojzjfoiqjyt.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
 };
-
-export default withPWA({
+// PWA 설정
+const buildConfig = withPWA({
   dest: 'public',
-  ...nextConfig,
-});
+  mode: 'production',
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
+export default buildConfig;
