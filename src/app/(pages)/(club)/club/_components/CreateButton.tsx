c9c45177e2cd33f95ed6892a779a66c6107511bf
jigong2024 <choijming21@naver.com>
2024-10-31 14:13:26 +0900

    bug: 모임생성 오류떠서 고침 #14

--- src/app/(pages)/(club)/club/_components/CreateButton.tsx
+++ src/app/(pages)/(club)/club/_components/CreateButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const CreateButton = () => {
   return (
-    <Link href={"/club/type-selection"} className="border-2 p-2 border-black">
+    <Link href={"/club"} className="border-2 p-2 border-black">
       생성
     </Link>
   );
