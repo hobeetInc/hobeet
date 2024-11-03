33925710885838f7f323cd52475ad11827d8ae1e
jigong2024 <choijming21@naver.com>
2024-11-03 15:42:31 +0900

    feat: 모임제목,모임소개 글자수 제한 #14

--- src/app/(pages)/(club)/club/_components/oneTimeClub/ClubTitle.tsx
+++ src/app/(pages)/(club)/club/_components/oneTimeClub/ClubTitle.tsx
@@ -6,6 +6,7 @@ const ClubTitle = ({ formData, setFormData }: OneTimeProps) => {
       <h1 className="mb-4">모임의 제목을 작성해 주세요</h1>
       <input
         type="text"
+        maxLength={36}
         value={formData.one_time_club_name}
         onChange={(e) =>
           setFormData({
@@ -15,6 +16,7 @@ const ClubTitle = ({ formData, setFormData }: OneTimeProps) => {
         }
         className="border-2 border-black mt-4 w-[358px] h-[48px] p-2"
       />
+      <div className="text-sm text-gray-500">{formData.one_time_club_name.length} / 36</div>
     </div>
   );
 };
