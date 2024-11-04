a402477a52fbc7d08dbe8e6cfeabcb7e032c7d90
Taeyeon <lty0314@gmail.com>
2024-11-05 03:22:07 +0900

    fix: 마이페이지 내가 결제한 목록 버그 수정 #55

--- src/app/(pages)/(mypage)/_components/eggPopPayDetail.tsx
+++ src/app/(pages)/(mypage)/_components/eggPopPayDetail.tsx
@@ -6,7 +6,7 @@ import { getEggPopPayList } from "../_api/supabase";
 import Image from "next/image";
 
 const EggPopPayDetail = () => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["eggPopPayData"],
     queryFn: getEggPopPayList
   });
@@ -49,26 +49,29 @@ const EggPopPayDetail = () => {
   if (isLoading) {
     return <div>로딩중...</div>;
   }
+
+  if (isError) {
+    return <div>에러 발생</div>;
+  }
   console.log(data);
 
   return (
     <div className="egg-day-pay-list">
       {data?.map((oneTimeClub, index) => (
         <div key={index} className="oneTimeClub-card my-4">
-          <div>{customDateFormat(oneTimeClub.o_t_c_kakaopay_create_at)}</div>
-          {oneTimeClub.o_t_c_id.map((idItem, idIndex) => (
-            <div key={idIndex}>
-              <div className="oneTimeClub-image">
-                <Image src={idItem.one_time_image} alt="payList" width={100} height={100} />
-              </div>
-              <div className="oneTimeClub-content">
-                <div className=" bg-gray-900 text-white text-xs px-2 py-1 rounded-full w-16 mt-2">에그팝</div>
-                <h3 className="oneTimeClub-title">{idItem.one_time_club_name}</h3>
-                <p className="oneTimeClub-location">{customAddress(idItem.one_time_club_location)}</p>
-                <p className="oneTimeClub-date">{customDate(idItem.one_time_club_date_time)}</p>
-              </div>
+          <div>{customDateFormat(oneTimeClub.o_t_c_id.one_time_club_date_time)}</div>
+
+          <div>
+            <div className="oneTimeClub-image">
+              <Image src={oneTimeClub.o_t_c_id.one_time_image} alt="payList" width={100} height={100} />
             </div>
-          ))}
+            <div className="oneTimeClub-content">
+              <div className=" bg-gray-900 text-white text-xs px-2 py-1 rounded-full w-16 mt-2">에그팝</div>
+              <h3 className="oneTimeClub-title">{oneTimeClub.o_t_c_id.one_time_club_name}</h3>
+              <p className="oneTimeClub-location">{customAddress(oneTimeClub.o_t_c_id.one_time_club_location)}</p>
+              <p className="oneTimeClub-date">{customDate(oneTimeClub.o_t_c_id.one_time_club_date_time)}</p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
