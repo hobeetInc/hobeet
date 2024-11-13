9e55aa8a2449d7c6becb7e76d38c5133d27f2480
Taeyeon <lty0314@gmail.com>
2024-11-14 00:19:58 +0900

    feat: 에그즈 관리 백버튼 추가

--- src/app/(pages)/(approvemembers)/approvemembers/[id]/page.tsx
+++ src/app/(pages)/(approvemembers)/approvemembers/[id]/page.tsx
@@ -3,9 +3,11 @@
 import { useState, useEffect } from "react";
 import { createClient } from "@/utils/supabase/client";
 import Image from "next/image";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { RegularClubApproveChatRoomRecruiterEntrance } from "@/app/(pages)/(chat)/_components/regularClub/RegularClubChatRoomRecruiterEntrance";
 import ApproveMemberTabBar from "@/components/uiComponents/ApproveMemberTapBar";
+import { HiOutlineChevronLeft } from "react-icons/hi";
+import Text from "@/components/uiComponents/TextComponents/Text";
 
 export interface ParticipationRequest {
   egg_club_participation_request_id: number;
@@ -64,6 +66,7 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({ isOpen, onClose, onConfirm 
 const ActiveMembersTab = ({ activeMembers }: { activeMembers: ParticipationRequest[] }) => {
   return (
     <div className="flex flex-col gap-6 ">
+
       {activeMembers.map((member, index) => (
         <div key={member.egg_club_participation_request_id} className="flex items-center gap-3">
           <Image
@@ -148,7 +151,7 @@ export default function ApproveMembersPage() {
   const supabase = createClient();
   const params = useParams();
   const clubId = Number(params.id);
-
+const router = useRouter();
   useEffect(() => {
     const fetchPendingAndActiveRequests = async () => {
       const { data: pendingData, error: pendingError } = await supabase
@@ -205,21 +208,38 @@ export default function ApproveMembersPage() {
       location.reload();
     }
   };
+  const handleBack = () =>{
+router.back();
+  }
 
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <h2 className="text-center h-12 text-gray-900 content-center font-bold font-pretendard mb-2">에그즈 관리</h2>
-      <ApproveMemberTabBar activeTab={activeTab} onTabChange={setActiveTab} value="egges" />
-      <div className="flex flex-col w-full mt-4 px-4">
-        <div className="text-left mb-2">
-          <span>총 {activeTab ? activeMembers.length : requests.length}명</span>
+      <div className="fixed top-0 right-0 left-0 flex w-full h-12 bg-white items-center">
+        <div className="left-0 m-3">
+          <div onClick={handleBack}>
+            <HiOutlineChevronLeft className="w-6 h-6" />
+          </div>
         </div>
-        <div>
-          {activeTab ? (
-            <ActiveMembersTab activeMembers={activeMembers} />
-          ) : (
-            <PendingRequestsTab requests={requests} onApprove={approveMember} />
-          )}
+        <div className="flex flex-grow justify-center">
+          <Text variant="header-16" className="text-gray-900">
+            에그즈 관리
+          </Text>
+        </div>
+        <div className="w-6 m-3"></div>
+      </div>
+      <div className="w-full mt-12">
+        <ApproveMemberTabBar activeTab={activeTab} onTabChange={setActiveTab} value="egges" />
+        <div className="flex flex-col w-full mt-4 px-4">
+          <div className="text-left mb-2">
+            <span>총 {activeTab ? activeMembers.length : requests.length}명</span>
+          </div>
+          <div>
+            {activeTab ? (
+              <ActiveMembersTab activeMembers={activeMembers} />
+            ) : (
+              <PendingRequestsTab requests={requests} onApprove={approveMember} />
+            )}
+          </div>
         </div>
       </div>
     </div>
