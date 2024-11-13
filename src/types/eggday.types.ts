1fcb65dbd95223c9e8d2c32264d61727735ef60c
jigong2024 <choijming21@naver.com>
2024-11-13 20:06:24 +0900

    fix: 유즈 파람 사용해서 뒤로가기 막기 수정 #76

--- src/types/eggday.types.ts
+++ src/types/eggday.types.ts
@@ -77,9 +77,15 @@ export interface EggDayMember {
   };
 }
 
+interface Params {
+  clubId: string;
+  dayId: string;
+}
+
 // 에그데이 헤더 props
 export type DayHeaderProps = {
   clubInfo: EggDayInfo | undefined;
+  params: Params;
 };
 
 // 에그데이 모달 props
