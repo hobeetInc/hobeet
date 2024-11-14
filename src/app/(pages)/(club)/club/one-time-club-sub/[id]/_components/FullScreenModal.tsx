81fd4cb44ab5adcd9159cf73f24e1f2f3c8f8a73
jigong2024 <choijming21@naver.com>
2024-11-15 01:03:14 +0900

    style: 전체 에그즈 페이지 css 수정 #124

--- src/app/(pages)/(club)/club/one-time-club-sub/[id]/_components/FullScreenModal.tsx
+++ src/app/(pages)/(club)/club/one-time-club-sub/[id]/_components/FullScreenModal.tsx
@@ -11,131 +11,74 @@ const FullScreenModal = ({ crewList, isOpen, onClose }: FullScreenModalProps) =>
 
   return (
     <div className="fixed inset-0 bg-white z-50">
-  
-        <div className="fixed flex items-center justify-between h-[60px] w-full px-4">
-          <button onClick={onClose} className="w-12 h-full flex items-center justify-center">
-            <IoIosArrowBack className="w-6 h-6" />
-          </button>
-          <div className="flex-1 text-center">
-            <Text variant="header-16">
-              전체 에그즈
-            </Text>
-          </div>
-          <div className="w-12"></div>
+      <div className="flex justify-center items-center w-full">
+        <div onClick={onClose} className="h-12 w-12 p-3 inline-flex">
+          <IoIosArrowBack className="w-6 h-6 cursor-pointer" />
         </div>
-
-
-        <div className="flex flex-col gap-8 mt-16">
-          <div className="px-4 h-[116px] flex-col justify-start items-start gap-4 inline-flex">
-            <div className="self-stretch h-6 flex-col justify-start items-start gap-2 flex">
-              <div className="self-stretch justify-start items-center gap-2 inline-flex">
-                <div className="grow shrink basis-0 text-[#0c0c0c] text-lg font-semibold font-['Pretendard'] leading-normal">
-                  결제 내역
-                </div>
-              </div>
-            </div>
-            <div className="self-stretch h-[76px] flex-col justify-start items-start gap-2 flex">
-              <div className="self-stretch justify-start items-center gap-4 inline-flex">
-                <div className="w-10 text-[#0c0c0c] text-sm font-semibold font-['Pretendard'] leading-[18.90px]">
-                  일시
-                </div>
-                <div className="w-[230px] text-[#0c0c0c] text-sm font-normal font-['Pretendard'] leading-tight">
-                  YYYY년 MM월 DD일(W) 오후 HH:MM{" "}
-                </div>
-              </div>
-              <div className="self-stretch justify-start items-center gap-4 inline-flex">
-                <div className="w-10 text-[#0c0c0c] text-sm font-semibold font-['Pretendard'] leading-[18.90px]">
-                  총 인원
-                </div>
-                <div className="w-[230px] text-[#0c0c0c] text-sm font-normal font-['Pretendard'] leading-tight">
-                  00명
-                </div>
-              </div>
-              <div className="self-stretch justify-start items-center gap-4 inline-flex">
-                <div className="w-10 text-[#0c0c0c] text-sm font-semibold font-['Pretendard'] leading-[18.90px]">
-                  총 금액
-                </div>
-                <div className="w-[230px] text-[#0c0c0c] text-sm font-normal font-['Pretendard'] leading-tight">
-                  10,000원
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="self-stretch h-[0px] border border-solid border-[#f2f2f2]"></div>
-
-          <div className="w-[390px] h-9 px-4 py-2 justify-start items-center gap-2.5 inline-flex">
-            <Text variant="subtitle-18">참여 에그즈</Text>
-          </div>
-
-          <div className="w-[390px] h-[616px] px-4 flex-col justify-start items-start gap-6 inline-flex">
-            {crewList?.map((member, index) => (
-              <div key={member.userId} className="self-stretch justify-start items-center gap-3 inline-flex">
-                <div className="relative w-[37px] h-[37px] overflow-hidden rounded-full">
-                  <Image
-                    src={member.userImage}
-                    alt={member.userName}
-                    width={37}
-                    height={37}
-                    className="w-full h-full object-cover border-2 border-black"
-                  />
-                </div>
-                <div className="w-[200px] flex-col justify-center items-start gap-1 inline-flex">
-                  <div className="self-stretch justify-start items-center gap-2 inline-flex">
-                    <div className="text-[#0c0c0c] text-base font-semibold font-['Pretendard'] leading-snug">
-                      {member.userName}
-                    </div>
-                    {index === 0 ? <Tag tagName="eggmaster" variant="black" /> : <Tag tagName="eggz" />}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+        <div className="flex-1 text-center pr-7">
+          <Text variant="header-16">참여 에그즈</Text>
         </div>
       </div>
 
+      <div className="flex flex-col gap-8 mt-16 w-ful justify-center items-center">
+        <div className="px-4 w-[390px] flex-col justify-start items-start gap-4 inline-flex">
+          <div className="self-stretch h-6 flex-col justify-start items-start gap-2 flex">
+            <div className="self-stretch justify-start items-center gap-2 inline-flex">
+              <div className="grow shrink basis-0 text-[#0c0c0c] text-lg font-semibold font-['Pretendard'] leading-normal">
+                결제 내역
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch h-[76px] flex-col justify-start items-start gap-2 flex">
+            <div className="self-stretch justify-start items-center gap-4 inline-flex">
+              <Text variant="body-14">일시</Text>
 
-    // <div className="fixed inset-0 bg-white z-50">
-    //   <div className="h-screen flex flex-col">
-    //     <div className="flex items-center justify-between h-[48px] p-4 relative">
-    //       <button onClick={onClose} className="absolute left-4">
-    //         <ChevronLeft />
-    //       </button>
-    //       <h1 className="flex-1 text-center text-lg font-semibold">참여 에그즈</h1>
-    //     </div>
+              <Text variant="subtitle-14">YYYY년 MM월 DD일(W) 오후 HH:MM </Text>
+            </div>
+            <div className="self-stretch justify-start items-center gap-4 inline-flex">
+              <Text variant="body-14">총 인원</Text>
 
-    //     <div className="flex-1 p-4 overflow-y-auto flex flex-col gap-10">
-    //       <div>
-    //         <h1 className="font-bold text-xl mb-4">결재 내역</h1>
-    //         <p>결재 내역 공간입니다</p>
-    //       </div>
-    //       <div>
-    //         <h1 className="font-bold text-xl mb-4">{`참여 에그즈 ${crewList.length}명`}</h1>
-    //         {crewList?.map((member, index) => (
-    //           <div key={member.userId} className="flex items-center gap-2 mb-4">
-    //             <div className="relative w-[37px] h-[37px] overflow-hidden rounded-full">
-    //               <Image
-    //                 src={member.userImage}
-    //                 alt={member.userName}
-    //                 width={37}
-    //                 height={37}
-    //                 className="w-full h-full object-cover border-2 border-black"
-    //               />
-    //             </div>
+              <Text variant="body-14">00명</Text>
+            </div>
+            <div className="self-stretch justify-start items-center gap-4 inline-flex">
+              <Text variant="subtitle-14">총 금액</Text>
 
-    //             <div className="flex flex-col gap-1 justify-center">
-    //               <div className="flex gap-2">
-    //                 <p>{member.userName}</p>
-    //                 <p className="text-[13px]">{index === 0 ? "에그장" : "에그즈"}</p>
-    //               </div>
-    //               <p className="text-[13px]">참여도</p>
-    //             </div>
-    //           </div>
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+              <Text variant="body-14">10,000원</Text>
+            </div>
+          </div>
+        </div>
+
+        <div className="self-stretch h-[0px] border border-solid border-gray-50"></div>
+
+        <div className="w-[390px] h-9 px-4 justify-start items-center inline-flex">
+          <Text variant="subtitle-18">참여 에그즈</Text>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-4">
+        <div className="w-[390px] px-4 flex-col justify-start items-start inline-flex gap-4">
+          {crewList?.map((member, index) => (
+            <div key={member.userId} className="self-stretch justify-start items-center gap-3 inline-flex">
+              <div className="relative w-[37px] h-[37px] overflow-hidden rounded-full">
+                <Image
+                  src={member.userImage}
+                  alt={member.userName}
+                  width={37}
+                  height={37}
+                  className="w-full h-full object-cover border-2 border-black"
+                />
+              </div>
+              <div className="w-[200px] flex-col justify-center items-start gap-1 inline-flex">
+                <div className="self-stretch justify-start items-center gap-2 inline-flex">
+                  <Text variant="subtitle-16">{member.userName}</Text>
+
+                  {index === 0 ? <Tag tagName="eggmaster" variant="black" /> : <Tag tagName="eggz" />}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/app/(pages)/(club)/club/regular-club-sub/[id]/_components/FullScreenModal.tsx
+++ src/app/(pages)/(club)/club/regular-club-sub/[id]/_components/FullScreenModal.tsx
@@ -21,32 +21,34 @@ const FullScreenModal = ({ crewList, isOpen, onClose }: FullScreenModalProps) =>
           </Text>
         </div>
 
-        <div className="w-[390px] h-9 px-4 py-2 justify-start items-center gap-2.5 inline-flex">
-          <Text variant="body_medium-14">{`총 ${crewList.length}명`}</Text>
-        </div>
+        <div className="w-full flex flex-col justify-center items-center gap-2 mt-2">
+          <div className="w-[390px] h-9 px-4 py-2 justify-start items-center gap-2.5 inline-flex">
+            <Text variant="body_medium-14">{`총 ${crewList.length}명`}</Text>
+          </div>
 
-        <div className="w-[390px] h-[616px] px-4 flex-col justify-start items-start gap-6 inline-flex">
-          {crewList?.map((member, index) => (
-            <div key={member.userId} className="self-stretch justify-start items-center gap-3 inline-flex">
-              <div className="relative w-[37px] h-[37px] overflow-hidden rounded-full">
-                <Image
-                  src={member.userImage}
-                  alt={member.userName}
-                  width={37}
-                  height={37}
-                  className="w-full h-full object-cover border-2 border-black"
-                />
-              </div>
-              <div className="w-[200px] flex-col justify-center items-start gap-1 inline-flex">
-                <div className="self-stretch justify-start items-center gap-2 inline-flex">
-                  <div className="text-[#0c0c0c] text-base font-semibold font-['Pretendard'] leading-snug">
-                    {member.userName}
+          <div className="w-[390px] px-4 flex-col justify-start items-start gap-6 inline-flex">
+            {crewList?.map((member, index) => (
+              <div key={member.userId} className="self-stretch justify-start items-center gap-3 inline-flex">
+                <div className="relative w-[37px] h-[37px] overflow-hidden rounded-full">
+                  <Image
+                    src={member.userImage}
+                    alt={member.userName}
+                    width={37}
+                    height={37}
+                    className="w-full h-full object-cover border-2 border-black"
+                  />
+                </div>
+                <div className="w-[200px] flex-col justify-center items-start gap-1 inline-flex">
+                  <div className="self-stretch justify-start items-center gap-2 inline-flex">
+                    <div className="text-[#0c0c0c] text-base font-semibold font-['Pretendard'] leading-snug">
+                      {member.userName}
+                    </div>
+                    {index === 0 ? <Tag tagName="eggmaster" variant="black" /> : <Tag tagName="eggz" />}
                   </div>
-                  {index === 0 ? <Tag tagName="eggmaster" variant="black" /> : <Tag tagName="eggz" />}
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
 
           {/* 
           <div key={member.userId} className="flex items-center gap-2 mb-4">
