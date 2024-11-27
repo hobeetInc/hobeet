bcd4fd9e7e5e89e71f0b40becda96ca4c33a17fd
Taeyeon <lty0314@gmail.com>
2024-11-27 22:59:57 +0900

    style: 모임원 정보에서 결제내역 일시 총인원 총금액 부분 삭제 #164

--- src/app/(pages)/(club)/club/one-time-club-sub/[id]/_components/FullScreenModal.tsx
+++ src/app/(pages)/(club)/club/one-time-club-sub/[id]/_components/FullScreenModal.tsx
@@ -22,42 +22,6 @@ const FullScreenModal = ({ crewList, isOpen, onClose }: FullScreenModalProps) =>
       </div>
 
       <div className="flex flex-col gap-8 mt-16">
-        <div className="px-4 h-[116px] flex-col justify-start items-start gap-4 inline-flex">
-          <div className="self-stretch h-6 flex-col justify-start items-start gap-2 flex">
-            <div className="self-stretch justify-start items-center gap-2 inline-flex">
-              <div className="grow shrink basis-0 text-[#0c0c0c] text-lg font-semibold font-['Pretendard'] leading-normal">
-                결제 내역
-              </div>
-            </div>
-          </div>
-          <div className="self-stretch h-[76px] flex-col justify-start items-start gap-2 flex">
-            <div className="self-stretch justify-start items-center gap-4 inline-flex">
-              <div className="w-10 text-[#0c0c0c] text-sm font-semibold font-['Pretendard'] leading-[18.90px]">
-                일시
-              </div>
-              <div className="w-[230px] text-[#0c0c0c] text-sm font-normal font-['Pretendard'] leading-tight">
-                YYYY년 MM월 DD일(W) 오후 HH:MM{" "}
-              </div>
-            </div>
-            <div className="self-stretch justify-start items-center gap-4 inline-flex">
-              <div className="w-10 text-[#0c0c0c] text-sm font-semibold font-['Pretendard'] leading-[18.90px]">
-                총 인원
-              </div>
-              <div className="w-[230px] text-[#0c0c0c] text-sm font-normal font-['Pretendard'] leading-tight">00명</div>
-            </div>
-            <div className="self-stretch justify-start items-center gap-4 inline-flex">
-              <div className="w-10 text-[#0c0c0c] text-sm font-semibold font-['Pretendard'] leading-[18.90px]">
-                총 금액
-              </div>
-              <div className="w-[230px] text-[#0c0c0c] text-sm font-normal font-['Pretendard'] leading-tight">
-                10,000원
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="self-stretch h-[0px] border border-solid border-[#f2f2f2]"></div>
-
         <div className="w-[390px] h-9 px-4 py-2 justify-start items-center gap-2.5 inline-flex">
           <Text variant="subtitle-18">참여 에그즈</Text>
         </div>
