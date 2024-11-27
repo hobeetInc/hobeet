5da1528b01d98f78e35f3f73be7cb63e3e1964f2
jigong2024 <choijming21@naver.com>
2024-11-27 20:46:31 +0900

    style: 채팅 탭바 마진 수정 #165

--- src/app/(pages)/(chat)/chat/page.tsx
+++ src/app/(pages)/(chat)/chat/page.tsx
@@ -8,7 +8,7 @@ const ChatPage = () => {
   const [chattingRooms, setChattingRooms] = useState(true);
   return (
     <>
-      <div className="flex  py-[var(--Font-size-100, 10px)] px-4 pt-[10px] border-b border-gray-200 justify-center items-center">
+      <div className="flex  py-[var(--Font-size-100, 10px)] border-b border-gray-200 justify-center items-center">
         <TabBar activeTab={chattingRooms} onTabChange={setChattingRooms} value="mychat" />
       </div>
       {chattingRooms ? <RegularClubChattingRoomPage /> : <OneTimeClubChattingRoomPage />}
